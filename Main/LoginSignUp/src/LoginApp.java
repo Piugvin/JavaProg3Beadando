e980a19bab0f4eb97f94f5e5815b845136a8d9ce
Piugvin <xgl3xi@hallgato.uniduna.hu>
2023-11-05 16:47:41 +0100

    formázás

--- Main/LoginSignUp/src/LoginApp.java
+++ Main/LoginSignUp/src/LoginApp.java
@@ -14,19 +14,18 @@ public class LoginApp {
         System.out.print("Password: ");
         String password = scanner.nextLine();
 
-        // Simulate checking login (in a real application, this would involve database authentication)
         if (isValidLogin(username, password)) {
             System.out.println("\nLogin successful. Welcome, " + username + "!");
-            // Perform actions after successful login
+
         } else {
             System.out.println("\nInvalid credentials. Login failed.");
-            // Handle failed login attempts
+
         }
     }
 
-    // Simulated method to check login (Replace this with actual authentication logic)
+
     private static boolean isValidLogin(String username, String password) {
-        // Simulated logic - replace this with actual authentication logic (e.g., database lookup)
+
         return username.equals("admin") && password.equals("password123");
     }
 }
--- Main/LoginSignUp/src/SignUpApp.java
+++ Main/LoginSignUp/src/SignUpApp.java
@@ -17,13 +17,11 @@ class SignUpApp {
         System.out.print("Email: ");
         String email = scanner.nextLine();
 
-        // Simulate storing user details (in a real application, this would be stored securely, e.g., in a database)
+
         System.out.println("\nThank you for signing up!");
         System.out.println("Your details:");
         System.out.println("Username: " + username);
-        System.out.println("Password: " + password); // Note: In a real application, never display passwords like this
+        System.out.println("Password: " + password);
         System.out.println("Email: " + email);
-
-        // Here, you would typically save the user details to a database or perform necessary actions for sign-up
     }
 }
