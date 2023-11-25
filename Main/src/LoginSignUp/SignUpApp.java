f9429d93f5701980ee6bf90bd9d269bf07be370c
Piugvin <rohoskadavid1@gmail.com>
2023-11-25 15:58:53 +0100

    used username

--- Main/src/LoginSignUp/SignUpApp.java
+++ Main/src/LoginSignUp/SignUpApp.java
@@ -2,7 +2,6 @@ package LoginSignUp;
 
 import Main.Main;
 import Snake.*;
-
 import java.awt.*;
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,7 +17,8 @@ public class SignUpApp {
     JTextField usernameField = new JTextField(20);
     JPasswordField passwordField = new JPasswordField(20);
     JPasswordField CpasswordField = new JPasswordField(20);
-    public  SignUpApp() {
+
+    public SignUpApp() {
         JFrame register = new JFrame("Regisztráció");
         //Background
         String absolutePath = "/Main/src/LoginSignUp/background.jpg";
@@ -32,7 +32,7 @@ public class SignUpApp {
             register.add(backgroundLabel);
         } catch (MalformedURLException e) {
             e.printStackTrace();
-            }
+        }
         //background
         JLabel usernameLabel = new JLabel("Felhasználónév:");
         JLabel passwordLabel = new JLabel("Jelszó:");
@@ -42,7 +42,6 @@ public class SignUpApp {
         JButton backButton = new JButton("Vissza");
 
         register.setLayout(null);
-
         register.add(usernameLabel);
         register.add(usernameField);
         register.add(passwordLabel);
@@ -54,16 +53,16 @@ public class SignUpApp {
         register.add(backButton);
 
         register.setSize(400, 600);
+
         usernameLabel.setBounds(150, 50, 200, 50);
         usernameField.setBounds(100, 100, 200, 50);
         passwordLabel.setBounds(179, 150, 200, 50);
         passwordField.setBounds(100, 200, 200, 50);
         CpasswordLabel.setBounds(141, 250, 200, 50);
         CpasswordField.setBounds(100, 300, 200, 50);
-        signUpButton.setBounds(100, 400, 200,50);
-        backButton.setBounds(100, 500, 200,50);
+        signUpButton.setBounds(100, 400, 200, 50);
+        backButton.setBounds(100, 500, 200, 50);
         register.setVisible(true);
-
         signUpButton.addActionListener(e -> signUp());
         backButton.addActionListener(e -> {
             register.dispose();
@@ -72,12 +71,31 @@ public class SignUpApp {
         register.setLocationRelativeTo(null);
         register.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    public static String epwd="", eusn="";
-    private void signUp() {
+
+    public static String epwd = "", eusn = "";
+    public class used {
+        public void showusrname() {
+            JFrame option = new JFrame("Már foglalt!");
+            JLabel already = new JLabel("Felhasználónév jelenleg foglalt!");
+            JButton ok = new JButton("OK!");
+            already.setForeground(Color.RED);
+            option.setLayout(null);
+            option.add(already);
+            option.add(ok);
+            option.setSize(300, 100);
+            already.setBounds(50, 1, 200, 20);
+            ok.setBounds(125, 36,30,20);
+            option.setLocationRelativeTo(null);
+            option.setVisible(true);
+            option.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            ok.addActionListener(e -> option.dispose());
+        }
+    }
+    private SignUpApp signUp() {
         String username = usernameField.getText();
         char[] password = passwordField.getPassword();
         char[] Cpassword = CpasswordField.getPassword();
-        if(Arrays.equals(Cpassword, password)) {
+        if (Arrays.equals(Cpassword, password)) {
             Connection connection;
             PreparedStatement preparedStatement;
             final Properties properties = new Properties();
@@ -87,25 +105,35 @@ public class SignUpApp {
                 String username1 = properties.getProperty("username");
                 String password1 = properties.getProperty("password");
                 connection = DriverManager.getConnection(url, username1, password1);
-                try {
-                    String sql = ("INSERT INTO users (username, password) VALUES (?,?)");
-                    preparedStatement = connection.prepareStatement(sql);
-                    preparedStatement.setString(1, username);
-                    preparedStatement.setString(2, Arrays.toString(password));
-                    int rowsAffected = preparedStatement.executeUpdate();
-                    if(rowsAffected > 0) {
-                        if (Arrays.equals(password, Cpassword)) {
-                             new GameFrame();
-                             eusn = username;
-                             epwd= Arrays.toString(password);
+                String query = "SELECT username FROM users WHERE username = ?";
+                preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, username);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                        SwingUtilities.invokeLater(() -> {
+                            used used = new used();
+                            used.showusrname();
+                        });
+                } else {
+                    try {
+                        String sql = ("INSERT INTO users (username, password) VALUES (?,?)");
+                        preparedStatement = connection.prepareStatement(sql);
+                        preparedStatement.setString(1, username);
+                        preparedStatement.setString(2, Arrays.toString(password));
+                        int rowsAffected = preparedStatement.executeUpdate();
+                        if (rowsAffected > 0) {
+                            new GameFrame();
+                            eusn = username;
+                            epwd = Arrays.toString(password);
                         }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
                     }
-            }catch (SQLException e) {
-                    e.printStackTrace();
                 }
-            } catch (SQLException | IOException e) {
-                throw new RuntimeException(e);
+            } catch (IOException | SQLException e) {
+            throw new RuntimeException(e);
             }
         }
+        return null;
     }
 }
