8b78370b154894693db984d6e2cc7df6c9c8603e
Piugvin <rohoskadavid1@gmail.com>
2023-11-26 18:20:44 +0100

    ok

--- Main/src/LoginSignUp/LoginApp.java
+++ Main/src/LoginSignUp/LoginApp.java
@@ -80,7 +80,7 @@ public class LoginApp {
                         option.add(ok);
                         option.setSize(300, 100);
                         already.setBounds(50, 1, 300, 20);
-                        ok.setBounds(125, 36, 40, 40);
+                        ok.setBounds(125, 36, 100, 50);
                         option.setLocationRelativeTo(null);
                         option.setVisible(true);
                         option.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
