22d39c006499d1b0cd8d30c60d627faf98d52e11
Piugvin <rohoskadavid1@gmail.com>
2023-11-26 15:44:30 +0100

    refactor,download

---
+++ Main/src/LoginSignUp/Accepted.java
@@ -0,0 +1,68 @@
+package LoginSignUp;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class Accepted {
+    public Accepted() {
+        System.out.println("Bejelentkezés sikeres!");
+        JFrame acceptedFrame = new JFrame("Adatok");
+        JLabel normalLabel = new JLabel("Visszafejtett jelszó:");
+        JLabel encryptLabel = new JLabel("Titkosított jelszó:");
+        JLabel jelszavak = new JLabel();
+        JButton button = new JButton("Jelszavak lekérése!");
+        String username = LoginApp.usernameField.getText();
+        final Properties properties = new Properties();
+        try (FileInputStream input = new FileInputStream("Main/src/Database Configuration/DB.properties")) {
+            properties.load(input);
+            String url = properties.getProperty("dburl");
+            String username1 = properties.getProperty("username");
+            String password1 = properties.getProperty("password");
+            try (Connection connection = DriverManager.getConnection(url, username1, password1)) {
+                String query = "SELECT password, Encrypted_password FROM users WHERE username = ?";
+                try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                    preparedStatement.setString(1, username);
+                    try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                        if (resultSet.next()) {
+                            String rowData = resultSet.getString("password") + "\t" + resultSet.getString("Encrypted_password");
+                            jelszavak.setText(rowData);
+                        }
+                    }
+                }
+            }
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        }
+        acceptedFrame.setLayout(null);
+        acceptedFrame.add(normalLabel);
+        acceptedFrame.add(encryptLabel);
+        acceptedFrame.add(jelszavak);
+        acceptedFrame.add(button);
+        acceptedFrame.setSize(300, 200);
+        acceptedFrame.setLocationRelativeTo(null);
+        normalLabel.setBounds(10, 50, 200, 20);
+        encryptLabel.setBounds(150, 50, 200, 20);
+        jelszavak.setBounds(50, 75, 300, 20);
+        button.setBounds(75, 150, 200, 50);
+        acceptedFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        acceptedFrame.setVisible(true);
+        button.addActionListener(a -> {
+            String filePath = "out/production/Main/LoginSignUp/file.txt";
+            try {
+                FileWriter fileWriter = new FileWriter(filePath);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                bufferedWriter.write(jelszavak.getText());
+                bufferedWriter.close();
+                System.out.println("Data has been exported to the file successfully.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
--- Main/src/LoginSignUp/LoginApp.java
+++ Main/src/LoginSignUp/LoginApp.java
@@ -1,15 +1,20 @@
 package LoginSignUp;
 
-import Main.*;
+import Main.Main;
+import Snake.GameFrame;
+
+import javax.swing.*;
+import java.awt.*;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
-import java.util.*;
-import javax.swing.*;
+import java.util.Arrays;
+import java.util.Properties;
 @SuppressWarnings({"InstantiationOfUtilityClass", "CallToPrintStackTrace"})
 public class LoginApp {
-    JTextField usernameField = new JTextField(20);
+    static JTextField usernameField = new JTextField(20);
     JPasswordField passwordField = new JPasswordField(20);
+
     public LoginApp() {
         JFrame login = new JFrame("Bejelentkezés");
 
@@ -32,8 +37,8 @@ public class LoginApp {
         usernameField.setBounds(100, 100, 200, 50);
         passwordLabel.setBounds(179, 150, 200, 50);
         passwordField.setBounds(100, 200, 200, 50);
-        loginButton.setBounds(100, 300, 200,50);
-        backButton.setBounds(100, 400, 200,50);
+        loginButton.setBounds(100, 300, 200, 50);
+        backButton.setBounds(100, 400, 200, 50);
 
         login.setVisible(true);
         loginButton.addActionListener(e -> login());
@@ -57,17 +62,38 @@ public class LoginApp {
             String username1 = properties.getProperty("username");
             String password1 = properties.getProperty("password");
             connection = DriverManager.getConnection(url, username1, password1);
-            try {
-                String sql = "SELECT users (username, password) VALUES (?,?)";
-                preparedStatement = connection.prepareStatement(sql);
-                preparedStatement.setString(1, username);
-                preparedStatement.setString(2, Arrays.toString(password));
-                preparedStatement.executeUpdate();
+            String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, username);
+                statement.setString(2, Arrays.toString(password));
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    if (resultSet.next()) {
+                        System.out.println("Bejelentkezés sikeres!");
+                        new Accepted();
+                    } else {
+                        JFrame option = new JFrame("Kérem próbálja újra!");
+                        JLabel already = new JLabel("Felhasználónév vagy a jelszó hibás!");
+                        JButton ok = new JButton("OK!");
+                        already.setForeground(Color.RED);
+                        option.setLayout(null);
+                        option.add(already);
+                        option.add(ok);
+                        option.setSize(300, 100);
+                        already.setBounds(50, 1, 300, 20);
+                        ok.setBounds(125, 36, 30, 20);
+                        option.setLocationRelativeTo(null);
+                        option.setVisible(true);
+                        option.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                        ok.addActionListener(a -> option.dispose());
+                    }
+                } catch (SQLException e) {
+                    System.out.println( e.getMessage());
+                }
             } catch (SQLException e) {
-                e.printStackTrace();
+                System.out.println( e.getMessage());
             }
-        } catch (IOException | SQLException e) {
-            throw new RuntimeException(e);
+        } catch (SQLException | IOException e) {
+            System.out.println( e.getMessage());
         }
     }
 }
